d27b688dfc472ede6da32db1d2157c56892fb2f4
yangchuan <190708321@qq.com>
2020-06-12 17:28:30 +0800

    more

--- pc-toutiao/app/src/components/Index.vue
+++ pc-toutiao/app/src/components/Index.vue
@@ -1,19 +1,16 @@
 <template>
     <div class='main'>
-        <more></more>
+        <Nav></Nav>
     </div>
 </template>
 <script>
 //这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
 //例如：import 《组件名称》 from '《组件路径》';
 import Nav from './nav/Nav.vue'
-import more from './yc/more.vue'
-
 export default {
 //import引入的组件需要注入到对象中才能使用
 components: {
-    Nav,
-    more,
+    Nav
 },
 data: function() {
 //这里存放数据
