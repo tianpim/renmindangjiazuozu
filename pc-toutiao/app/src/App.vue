2c0a20c5379a6715f3d782e736a6925b283830dc
tianpim <1803736469@qq.com>
2020-06-18 11:07:55 +0800

    2020-06-18

--- pc-toutiao/app/src/App.vue
+++ pc-toutiao/app/src/App.vue
@@ -5,22 +5,10 @@
 </template>
 
 <script>
-import Index from "./components/Index.vue"
-import Login from "./components/Login.vue"
-import Register from "./components/Register.vue"
-import UserCenter from "./components/UserCenter.vue"
-import UserData from "./components/UserData.vue"
-import Detail from "./components/Detail.vue"
 
 export default {
   name: 'App',
   components: {
-    Index,
-    Login,
-    Register,
-    UserCenter,
-    UserData,
-    Detail
   }
 }
 </script>
